804750de728c810b84a379014e06a481bedc512a
rifkiii <33884039+rifkiii@users.noreply.github.com>
2018-02-05 10:09:30 +0800

    UPDATE ENUM

--- Exercise-Enum.cpp
+++ Exercise-Enum.cpp
@@ -1,11 +1,12 @@
+#include<iostream>
+using namespace std;
 
+enum week {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
 
-//if the day is sunday, what day is it?
-//write your code using enum and Sunday is your default 'day'
-
-minggu hariIni;
-
-cout<<"Hari ke-"hariIni+1;
-return 0;
-
-
+int main ()
+{
+	week today;
+	today = Wednesday;
+	cout<<"Day"<<today+1;
+	return 0;
+}
